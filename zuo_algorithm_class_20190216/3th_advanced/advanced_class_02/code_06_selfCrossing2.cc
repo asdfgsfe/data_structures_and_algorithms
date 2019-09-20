f845557418ad1f9e949edbb675fb059b851d4b18
unknown <gaowei1@360.cn>
2019-09-20 19:16:55 +0800

    restudy process 3th advanced 07 2019-09-20 19:16

--- zuo_algorithm_class_20190216/3th_advanced/advanced_class_02/code_06_selfCrossing2.cc
+++ zuo_algorithm_class_20190216/3th_advanced/advanced_class_02/code_06_selfCrossing2.cc
@@ -15,11 +15,14 @@ bool IsSelfCorssing(const vector<int>& s)
 	}
 	for (int i = 5; i < s.size(); ++i)
 	{
-		if (x[i - 1] < x[i - 3] && x[i] >= x[2])
+		if (x[i - 1] < x[i - 3] && x[i] >= x[i - 2])
 		{
 			return true;
 		}
-		if (x[i - 1] < x[i - 3] && x[i - 2] >= x[i - 4] && x[i - 1] + x[i - 5] >= x[i - 3] && x[i] + x[i - 4] >= x[i - 2])
+		if (x[i - 1] < x[i - 3] 
+			&& x[i - 2] >= x[i - 4] 
+			&& (x[i - 1] + x[i - 5] >= x[i - 3])
+			&& x[i] + x[i - 4] >= x[i - 2])
 		{
 			return true;
 		}
